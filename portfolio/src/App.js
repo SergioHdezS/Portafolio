e12074f2f88812e2c267ca5131a94db454b7aaca
SergioHdezS <sergiohdez.dev@gmail.com>
2023-01-19 19:16:04 -0300

    avances portafolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className='main'>
+        <Home />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <section className='home section' id='home'>
+            <div className="home-container container grid">
+                <div className="home-content grid">
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home
