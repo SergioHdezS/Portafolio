90bef723b840d3f01d26d54ea9351daa7101e3b7
SergioHdezS <sergiohdez.dev@gmail.com>
2023-01-19 20:43:53 -0300

    avances portafolio

---
+++ portfolio/src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home-social">
+            <a href="https://www.instagram.com/" className="home-social-icon" target="_blank">
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://dribbble.com/" className="home-social-icon" target="_blank">
+                <i className="uil uil-dribbble"></i>
+            </a>
+                
+            <a href="https://github.com/" className="home-social-icon" target="_blank">
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
